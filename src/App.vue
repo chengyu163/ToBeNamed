23657b388e00fcff9213aebb38f4dabea390bad7
cheng程 <chengyu163@gmail.com>
2021-01-07 22:08:56 +0000

    upload

---
+++ src/App.vue
@@ -0,0 +1,239 @@
+<template>
+    <div id="app">
+        <StyleEditor ref="styleEditor" :code="currentStyle"></StyleEditor>
+        <ResumeEditor ref="resumeEditor" :markdown="currentMarkdown" :enableHtml="enableHtml"></ResumeEditor>
+    </div>
+</template>
+<script>
+import StyleEditor from './components/StyleEditor'
+import ResumeEditor from './components/ResumeEditor'
+import './assets/reset.css'
+let isPc = (function() {
+    var userAgentInfo = navigator.userAgent;
+    var Agents = ["Android", "iPhone",
+        "SymbianOS", "Windows Phone",
+        "iPad", "iPod"
+    ];
+    var flag = true;
+    for (var v = 0; v < Agents.length; v++) {
+        if (userAgentInfo.indexOf(Agents[v]) > 0) {
+            flag = false;
+            break;
+        }
+    }
+    return flag;
+}());
+let getDateDiff = function(startDate, endDate) {
+    var startTime = new Date(Date.parse(startDate.replace(/-/g, "/"))).getTime();
+    var endTime = new Date(Date.parse(endDate.replace(/-/g, "/"))).getTime();
+    var dates = Math.abs((startTime - endTime)) / (1000 * 60 * 60 * 24);
+    return dates;
+}
+document.title += getDateDiff((new Date()).getFullYear() + '-' + ((new Date()).getMonth() + 1) + '-' + (new Date()).getDate(), '2016-09-15') + 1 + '天';
+export default {
+    name: 'app',
+    components: {
+        StyleEditor,
+        ResumeEditor
+    },
+    data() {
+        return {
+            interval: 27,
+            currentStyle: '',
+            enableHtml: false,
+            fullStyle: [
+                `/*
+* Hi。佳琪！
+* 认识两个月，我还没有为你做过什么。
+* 我想我们都作为工程师， 我就做一些不一样的东西吧
+* 我拿了一个template， 然后做了这个网站
+* 他将作为我们认识的缘分
+* 以后我会定期的在里面加入我们的内容
+* 记录我们的一切
+* 你现在用的是。。。${isPc ? '电脑' : '手机'}
+*/
+
+/* 首先给所有元素加上过渡效果 */
+* {
+  -webkit-transition: all .3s;
+  transition: all .3s;
+}
+/* 白色背景太单调了。你那么可爱就用粉色吧 */
+html {
+  color: rgb(0,0,0);
+  background: rgb(255,192,203);
+}
+/* 文字太近了 */
+.styleEditor {
+  padding: .5em;
+  border: 1px solid;
+  margin: .5em;
+  overflow: auto;
+  ${ isPc ? 'width: 48%;height: 96%;' : 'width: 96%;height: 50%;' }
+  font-size: 14px;
+  line-height:1.5;
+}
+/* 这些代码颜色都一样。加点儿高亮区别来 */
+.token.selector{ color: rgb(133,153,0) }
+.token.property{ color: rgb(187,137,0) }
+.token.punctuation{ color: yellow }
+.token.function{ color: rgb(42,161,152) }
+.token.comment{ color: rgb(177,177,177) }
+/* 加个 3D 效果 */
+html{
+  -webkit-perspective: 1000px;
+          perspective: 1000px;
+}
+.styleEditor {
+  position: fixed;
+  ${ isPc ? 'left: 0;' : 'left:0;right:0;margin:auto;'}
+  top: 0;
+  -webkit-transition: none;
+  transition: none;
+  ${ isPc ? '-webkit-transform: rotateY(10deg) translateZ(-100px) ;transform: rotateY(10deg) translateZ(-100px) ;' : '-webkit-transform: rotateX(-10deg) translateZ(-100px) ;transform: rotateX(-10deg) translateZ(-100px) ;' }
+  ${ isPc ? '' : '-webkit-transform-origin: 50% 0% 0;transform-origin: 50% 0% 0;' }
+}
+
+/* 再来一张信纸 */
+.resumeEditor{
+  position: fixed;
+  ${ isPc ? 'right: 0;' : 'left:0;right:0;margin:auto;'}
+  ${ isPc ? 'top: 0;' : 'bottom:2%;'}
+  padding: .5em;
+  ${ isPc ? 'margin: .5em;' : ''}
+  ${ isPc ? 'width: 48%;height: 96%;' : 'width: 96%;height: 50%;' }
+  border: 1px solid;
+  color: #222;
+  overflow: auto;
+  font-size: 14px;
+  line-height:1.5;
+  ${ isPc ? '-webkit-transform: rotateY(-10deg) translateZ(-100px) ;transform: rotateY(-10deg) translateZ(-100px) ;' : '-webkit-transform: rotateX(10deg) translateZ(-100px) ;transform: rotateX(10deg) translateZ(-100px) ;' }
+    ${ isPc ? '' : '-webkit-transform-origin: 50% 0% 0;transform-origin: 50% 0% 0;' }
+  }
+/* 我开始写了 */
+
+`
+            ],
+            currentMarkdown: '',
+            fullMarkdown: `Yu Cheng × Jiqi Wang
+----
+
+2020年11月10日。第一次加你微信。帮你解答了iac的问题（虽然当时我iac已经差不多全忘了 xD）。
+2020年11月17日。我们经历了一些不愉快，可最终我还是相信你了。
+2020年12月初。 我把你删了，但心里还是惦记着你
+2020年12月底。 我们重新有了联系，这次的感情比以往好多了
+2020年12月05日。 我和你谈了很久心，很感谢你对我坦诚。 过去的事情，都不重要。
+2020年12月06日.  我之所以在意是因为我想和你走到最后。
+我知道你会有顾虑，但其实我已经想好了，以后你只会是我的就够了。我欠你一个告白，他将会是在接下来的日子里
+
+认识已有 \`${getDateDiff((new Date()).getFullYear()+'-'+((new Date()).getMonth()+1)+'-'+(new Date()).getDate(),'2020-11-10')}\` 天
+
+一起呲过的餐厅
+----
+* 还没xD
+
+
+一起看过的电影
+----
+1. 还没xD
+
+
+一起玩过的地方
+----
+* 还没xD
+
+
+一起玩过的游戏
+----
+1. LOL
+
+
+> 【fuck this world】
+> 咳咳咳。我不只想出现在你的世界里。我还要去改变你的人生。！
+
+`
+        }
+    },
+    created() {
+        this.makeResume()
+    },
+
+    methods: {
+        makeResume: async function() {
+            await this.progressivelyShowStyle(0)
+            await this.progressivelyShowResume()
+            await this.progressivelyShowStyle(1)
+            await this.showHtml()
+            await this.progressivelyShowStyle(2)
+        },
+        showHtml: function() {
+            return new Promise((resolve, reject) => {
+                this.enableHtml = true
+                resolve()
+            })
+        },
+        progressivelyShowStyle(n) {
+            return new Promise((resolve, reject) => {
+                let interval = this.interval
+                let showStyle = (async function() {
+                    let style = this.fullStyle[n]
+                    if (!style) {
+                        return
+                    }
+                    // 计算前 n 个 style 的字符总数
+                    let length = this.fullStyle.filter((_, index) => index <= n).map((item) => item.length).reduce((p, c) => p + c, 0)
+                    let prefixLength = length - style.length
+                    if (this.currentStyle.length < length) {
+                        let l = this.currentStyle.length - prefixLength
+                        let char = style.substring(l, l + 1) || ' '
+                        this.currentStyle += char
+                        if (style.substring(l - 1, l) === '\n' && this.$refs.styleEditor) {
+                            this.$nextTick(() => {
+                                this.$refs.styleEditor.goBottom()
+                            })
+                        }
+                        setTimeout(showStyle, interval)
+                    } else {
+                        resolve()
+                    }
+                }).bind(this)
+                showStyle()
+            })
+        },
+        progressivelyShowResume() {
+            return new Promise((resolve, reject) => {
+                let length = this.fullMarkdown.length
+                let interval = this.interval
+                let showResume = () => {
+                    if (this.currentMarkdown.length < length) {
+                        this.currentMarkdown = this.fullMarkdown.substring(0, this.currentMarkdown.length + 1)
+                        let lastChar = this.currentMarkdown[this.currentMarkdown.length - 1]
+                        let prevChar = this.currentMarkdown[this.currentMarkdown.length - 2]
+                        if (prevChar === '\n' && this.$refs.resumeEditor) {
+                            this.$nextTick(() => this.$refs.resumeEditor.goBottom())
+                        }
+                        setTimeout(showResume, interval)
+                    } else {
+                        resolve()
+                    }
+                }
+                showResume()
+            })
+        }
+    }
+}
+</script>
+<style scoped>
+#app {
+    font-family: 'Avenir', Helvetica, Arial, sans-serif;
+    -webkit-font-smoothing: antialiased;
+    -moz-osx-font-smoothing: grayscale;
+}
+
+html {
+    min-height: 100%;
+}
+.styleEditor {
+    -webkit-backface-visibility: hidden;
+}
+</style>
